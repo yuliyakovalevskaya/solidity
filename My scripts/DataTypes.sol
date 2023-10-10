1935eb4ec4a942994921d260010faa8b97afb19e
yuliyakovalevskaya <y.i.kovalevskaya@gmail.com>
2023-10-10 18:50:06 +0530

    Added DataTypes

---
+++ My scripts/DataTypes.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract DataTypes {
+    bool public boo = true;
+
+    uint8 public u8 = 12;
+    uint public u = 1314;
+    uint public u1 = 2345;
+}
+
